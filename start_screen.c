2d27fe46b4a21a4201fa30be15ef92c1f9b6008b
Shadman Kaif <kaifshadman@gmail.com>
2020-04-03 01:42:55 -0400

    start screen disappears after key 0 is pressed

--- start_screen.c
+++ start_screen.c
@@ -8,19 +8,62 @@ void clear_screen();
 void draw_line(int x0, int y0, int x1, int y1, short int color);
 void plot_pixel(int x, int y, short int line_color);
 
+void disable_A9_interrupts(void);
+void set_A9_IRQ_stack(void);
+void config_GIC(void);
+void config_KEYs(void);
+void enable_A9_interrupts(void);
+
+// Draw a box at (x,y) location
+void draw_box(int x, int y, int width, int length, short int color){
+    for(int i = y; i < y+length ; ++i)
+        for (int j = x; j < x+width ; ++j)
+        plot_pixel(j, i, color);
+}   
+
+/********************************************************************
+* Pushbutton - Interrupt Service Routine
+*
+* This routine checks which KEY has been pressed. It writes to HEX0
+*******************************************************************/
+void pushbutton_ISR(void) {
+	/* KEY base address */
+	volatile int * KEY_ptr = (int *) 0xFF200050;
+	int press;
+	press = *(KEY_ptr + 3); // read the pushbutton interrupt register
+	*(KEY_ptr + 3) = press; // Clear the interrupt
+	if (press & 0x1) { // KEY0
+		clear_screen();
+	}
+	else if (press & 0x2) { //KEY1
+
+	}
+	else if (press & 0x4) { //KEY2
+
+	}
+	else { //KEY3
+
+	}
+	return;
+}
+
 
 int main(void){
 	volatile int* pixel_ctrl_ptr = (int*) 0xFF203020;
 	/* Read location of the pixel buffer from the pixel buffer controller */
 	pixel_buffer_start = *pixel_ctrl_ptr;
-
 	clear_screen();
-	
 	start_message();
+	
+	disable_A9_interrupts(); // disable interrupts in the A9 processor
+	set_A9_IRQ_stack(); // initialize the stack pointer for IRQ mode
+	config_GIC(); // configure the general interrupt controller
+	config_KEYs(); // configure pushbutton KEYs to generate interrupts
+	enable_A9_interrupts(); // enable interrupts in the A9 processor
+	while (1); // wait for an interrupt
+	
 	return 0;
 }
-
-
 
 // Series of strings that are displayed at the start screen
 void start_message(){
@@ -64,7 +107,7 @@ void start_message(){
 	 fifth_string++;
    }
 	
-   char* sixth_string = "Press Enter to Continue...";
+   char* sixth_string = "Press KEY0 to Continue...";
    x = 153;
    while (*sixth_string) {
      write_char(x, 32, *sixth_string);
@@ -92,6 +135,12 @@ void clear_screen() {
 	for (x = 0; x < 320; x++){
 		for (y = 0; y < 240; y++){
 			plot_pixel(x, y, 0x0000);
+		}
+	}
+	
+	for (x = 0; x < 80; x++){
+		for (y = 0; y < 60; y++){
+			write_char(x, y, ' ');
 		}
 	}
 }
@@ -151,4 +200,127 @@ void draw_line(int x0, int y0, int x1, int y1, short int color){
 			error = error - deltax;
 		}
 	}
+}
+
+/* setup the KEY interrupts in the FPGA */
+void config_KEYs() {
+	volatile int * KEY_ptr = (int *) 0xFF200050; // pushbutton KEY base address
+	*(KEY_ptr + 2) = 0xF; // enable interrupts for the two KEYs
+}
+
+void pushbutton_ISR(void);
+void config_interrupt(int, int);
+
+// Define the IRQ exception handler
+void __attribute__((interrupt)) __cs3_isr_irq(void) {
+
+	// Read the ICCIAR from the CPU Interface in the GIC
+	int interrupt_ID = *((int *)0xFFFEC10C);
+
+	if (interrupt_ID == 73) // check if interrupt is from the KEYs
+	pushbutton_ISR();
+		else {
+	while (1); // if unexpected, then stay here
+	}
+			
+	// Write to the End of Interrupt Register (ICCEOIR)
+	*((int *)0xFFFEC110) = interrupt_ID;
+}
+
+// Define the remaining exception handlers
+void __attribute__((interrupt)) __cs3_reset(void) {
+	while (1);
+}
+
+void __attribute__((interrupt)) __cs3_isr_undef(void) {
+	while (1);
+}
+
+void __attribute__((interrupt)) __cs3_isr_swi(void) {
+	while (1);
+}
+
+void __attribute__((interrupt)) __cs3_isr_pabort(void) {
+	while (1);
+}
+
+void __attribute__((interrupt)) __cs3_isr_dabort(void) {
+	while (1);
+}
+
+void __attribute__((interrupt)) __cs3_isr_fiq(void) {
+	while (1);
+}
+
+/*
+* Turn off interrupts in the ARM processor
+*/
+void disable_A9_interrupts(void) {
+	int status = 0b11010011;
+	asm("msr cpsr, %[ps]" : : [ps] "r"(status));
+}
+
+/*
+* Initialize the banked stack pointer register for IRQ mode
+*/
+void set_A9_IRQ_stack(void) {
+	int stack, mode;
+	stack = 0xFFFFFFFF - 7; // top of A9 onchip memory, aligned to 8 bytes
+	/* change processor to IRQ mode with interrupts disabled */
+	mode = 0b11010010;
+	asm("msr cpsr, %[ps]" : : [ps] "r"(mode));
+	/* set banked stack pointer */
+	asm("mov sp, %[ps]" : : [ps] "r"(stack));
+		/* go back to SVC mode before executing subroutine return! */
+	mode = 0b11010011;
+	asm("msr cpsr, %[ps]" : : [ps] "r"(mode));
+}
+
+/*
+* Turn on interrupts in the ARM processor
+*/
+void enable_A9_interrupts(void) {
+	int status = 0b01010011;
+	asm("msr cpsr, %[ps]" : : [ps] "r"(status));
+}
+
+/*
+* Configure the Generic Interrupt Controller (GIC)
+*/
+void config_GIC(void) {
+	config_interrupt (73, 1); // configure the FPGA KEYs interrupt (73)
+	// Set Interrupt Priority Mask Register (ICCPMR). Enable interrupts of all
+	// priorities
+	*((int *) 0xFFFEC104) = 0xFFFF;
+	// Set CPU Interface Control Register (ICCICR). Enable signaling of interrupts
+	*((int *) 0xFFFEC100) = 1;
+	// Configure the Distributor Control Register (ICDDCR) to send pending interrupts to CPUs
+	*((int *) 0xFFFED000) = 1;
+}
+
+/*
+* Configure Set Enable Registers (ICDISERn) and Interrupt Processor Target
+* Registers (ICDIPTRn). The default (reset) values are used for other registers
+* in the GIC.
+*/
+void config_interrupt(int N, int CPU_target) {
+	int reg_offset, index, value, address;
+	/* Configure the Interrupt Set-Enable Registers (ICDISERn).
+	* reg_offset = (integer_div(N / 32) * 4
+	* value = 1 << (N mod 32) */
+	reg_offset = (N >> 3) & 0xFFFFFFFC;
+	index = N & 0x1F;
+	value = 0x1 << index;
+	address = 0xFFFED100 + reg_offset;
+	/* Now that we know the register address and value, set the appropriate bit */
+	*(int *)address |= value;
+	/* Configure the Interrupt Processor Targets Register (ICDIPTRn)
+	* reg_offset = integer_div(N / 4) * 4
+	* index = N mod 4 */
+	reg_offset = (N & 0xFFFFFFFC);
+	index = N & 0x3;
+	address = 0xFFFED800 + reg_offset + index;
+	/* Now that we know the register address and value, write to (only) the
+	* appropriate byte */
+	*(char *)address = (char)CPU_target;
 }
