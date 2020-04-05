bba666dae282a91db5f7f26f9f4937ffca932852
Shadman Kaif <kaifshadman@gmail.com>
2020-04-05 00:18:22 -0400

    added road background in the car.c file

--- car.c
+++ car.c
@@ -20,8 +20,15 @@ void draw_background();
 void draw_taxi(int x, int y);
 void draw_car(int x, int y, short int colour);
 
+void draw_line(int x0, int y0, int x1, int y1, short int color);
 
 
+//Used to swap the values of x and y when requires in draw_line function
+void swap(int *x, int *y){
+	int temp = *x;
+    *x = *y;
+    *y = temp;   
+}
 
 // function to sync entire screen
 void wait_for_vsync(){
@@ -319,15 +326,83 @@ void draw_car(int x, int y, short int colour) {
 
 
 
-draw_background() {
+void draw_background() {
 
-    // draw road colour
-    draw_box(0, 0, 320, 240, ROAD_GREY);
+	// First pair of dotted lines
+	draw_line(52, 48, 52, 96, 0xFFFF);
+	draw_line(52, 144, 52, 192, 0xFFFF);
 
+	// First big lane
+	draw_line(103, 0, 103, 239, 0xFFFF);
+	draw_line(104, 0, 104, 239, 0xFFFF);
+	
+	// Second pair of dotted lines
+	draw_line(156, 48, 156, 96, 0xFFFF);
+	draw_line(156, 144, 156, 192, 0xFFFF);
 
+	// Second big lane
+	draw_line(209, 0, 209, 239, 0xFFFF);
+	draw_line(210, 0, 210, 239, 0xFFFF);
+	
+	// Third pair of dotted lines
+	draw_line(262, 48, 262, 96, 0xFFFF);
+	draw_line(262, 144, 262, 192, 0xFFFF);
 }
 
 
+
+void draw_line(int x0, int y0, int x1, int y1, short int color){
+	bool is_steep = abs(y1 - y0) > abs(x1 - x0);
+
+	if (is_steep){
+		swap(&x0, &y0);
+		swap(&x1, &y1);
+	}
+
+	if (x0 > x1){
+		swap(&x0, &x1);
+		swap(&y0, &y1);
+	}
+
+	int deltax = x1 - x0;
+	int deltay = y1 - y0;
+
+	if (deltay < 0){
+		deltay = -deltay;
+	}
+
+	//The error variable takes into account the relative difference between 
+	//the width (deltax) and height of the line (deltay) in deciding how often y 
+	//should beincremented. 
+	int error = -(deltax / 2);
+
+	int y = y0;
+	int y_step;
+
+	if (y0 < y1){
+		y_step = 1;
+	}
+
+	else{
+		y_step = -1;
+	}
+
+	for (int x = x0; x <= x1; x++){
+		if (is_steep){
+			plot_pixel(y, x, color);
+		}
+		else{
+			plot_pixel(x, y, color);
+		}
+
+		error = error + deltay;
+
+		if (error >= 0){
+			y = y + y_step;
+			error = error - deltax;
+		}
+	}
+}
 
 
 
